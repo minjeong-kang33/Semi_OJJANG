7e65f62f21e68cafd36b24a7afb7f7ced029d6f4
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-02-16 09:09:21 +0900

    [23.02.16] like

---
+++ src/main/java/com/itwillbs/like/action/LikePro.java
@@ -0,0 +1,17 @@
+package com.itwillbs.like.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.itwillbs.member.action.Action;
+import com.itwillbs.member.action.ActionForward;
+
+public class LikePro implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
