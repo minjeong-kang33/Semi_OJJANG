2ce86bd861f4bdfa8259cc30cd79204d4a290ff1
syh <syh940323@naver.com>
2023-02-17 19:23:19 +0900

    main.jsp if문으로 검색시에만 나타날 수 있게 페이징처리 Member컨트롤러 - Main.pro > 새로 생성한 searchPro에 담아서 보완해서 호출 SellDAO - 검색매서드 getsellsearch 추가

---
+++ src/main/java/com/itwillbs/member/action/SearchPro.java
@@ -0,0 +1,62 @@
+package com.itwillbs.member.action;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.itwillbs.sell.db.SellDAO;
+import com.itwillbs.sell.db.SellDTO;
+public class SearchPro implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+				request.setCharacterEncoding("UTF-8");
+				String search =request.getParameter("search"); 
+				SellDAO dao=new SellDAO();
+				int pageSize=9;
+				String pageNum=request.getParameter("pageNum");
+				if(pageNum==null){
+					pageNum="1";		
+				}
+				int currentPage=Integer.parseInt(pageNum);				
+				int startRow=(currentPage-1)/pageSize*pageSize+1;
+				int endRow=startRow+pageSize-1;
+				
+	
+		
+				int pageBlock=10;
+				int startPage=(currentPage-1)/pageBlock*pageBlock+1;
+				int endPage=startPage+pageBlock-1;
+				int count = dao.getSellBoardCount();
+				int pageCount=count/pageSize+(count%pageSize==0?0:1);
+				if(endPage > pageCount){
+					endPage = pageCount;
+				}
+				
+				ArrayList<SellDTO> sellList = null;
+				if(search!=null){
+					sellList = dao.getsellSearch(startRow, pageSize, search);
+				}else{
+					 sellList = dao.getsellList(startRow, pageSize);
+				}
+				
+				// request 가져온 데이터 담기
+				request.setAttribute("sellList", sellList);
+				request.setAttribute("currentPage", currentPage);
+				request.setAttribute("startPage", startPage);
+				request.setAttribute("pageBlock", pageBlock);
+				request.setAttribute("pageSize", pageSize);
+				request.setAttribute("endPage", endPage);
+				request.setAttribute("pageCount", pageCount);
+				
+				
+			
+				// 이동
+				ActionForward forward=new ActionForward();
+				forward.setPath("home/main.jsp");
+				forward.setRedirect(false);
+				return forward;
+			}
+		}
+	
