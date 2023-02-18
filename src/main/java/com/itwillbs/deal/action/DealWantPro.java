cd9e74a8990b0c62e63d5312d27705c4be231aa0
PC <PC@DESKTOP-RPTJ6AG>
2023-02-18 15:45:08 +0900

    [23.02.18] DealDAO.java 수정 DealWantPro.java 수정 EndDealPro.java 수정 LikePro.java 수정 MypagaeDAO.java 수정 profileUpdate.jsp 수정

--- src/main/java/com/itwillbs/deal/action/DealWantPro.java
+++ src/main/java/com/itwillbs/deal/action/DealWantPro.java
@@ -20,28 +20,35 @@ public class DealWantPro implements Action {
 		
 		request.setCharacterEncoding("utf-8");
 		HttpSession session=request.getSession();
-		String M_id=(String)session.getAttribute("M_id");
+		String M_id=(String)session.getAttribute("M_id");//구매희망자
 		int S_num=Integer.parseInt(request.getParameter("S_num"));
-		String M_ids=request.getParameter("M_id");
+		String M_ids=request.getParameter("M_id");//판매자
 		
-		SellDTO dto=new SellDTO();
+//		SellDTO dto=new SellDTO();
 		DealDAO dao=new DealDAO();
 		DealDTO Dto=dao.CheckDeal(M_id, S_num);
-		
+		DealDTO Dto2=dao.CheckDeal2(M_ids, S_num);
 		
 		ActionForward forward=null;
 		
-		if(Dto==null){	
+		if(Dto2!=null) { //이미 거래가 완료된 상품인지 확인
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out=response.getWriter(); //프린트라이트에 쓰기 권한을 받아서 리스폰스 어쩌구에 담음
+			out.println("<script type='text/javascript'>");
+			out.println("alert('이미 거래가 완료된 상품입니다.')");
+			out.println("history.back()");
+			out.println("</script>");
+			out.close();	
+		}else if(Dto==null){ //거래희망을 눌렀었는지 확인하고 deal로 insert
 			dao.insertDeal(S_num, M_id, M_ids);	
 			response.setContentType("text/html; charset=UTF-8");
 			PrintWriter out=response.getWriter(); //프린트라이트에 쓰기 권한을 받아서 리스폰스 어쩌구에 담음
 			out.println("<script type='text/javascript'>");
-			out.println("alert('거래신청 완료')");
+			out.println("alert('거래 신청 완료')");
 			out.println("history.back()");
 			out.println("</script>");
-			out.close();		 
-					
-		}else{
+			out.close();		 				
+		}else if(Dto!=null){ 
 			response.setContentType("text/html; charset=UTF-8");
 			PrintWriter out=response.getWriter(); //프린트라이트에 쓰기 권한을 받아서 리스폰스 어쩌구에 담음
 			out.println("<script type='text/javascript'>");
@@ -52,6 +59,8 @@ public class DealWantPro implements Action {
 			forward=null;
 		 }
 
+	
+		
 		return forward;
 
 	}
--- src/main/java/com/itwillbs/deal/action/EndDealPro.java
+++ src/main/java/com/itwillbs/deal/action/EndDealPro.java
@@ -4,6 +4,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.itwillbs.deal.db.DealDAO;
 import com.itwillbs.deal.db.DealDTO;
@@ -14,15 +15,19 @@ public class EndDealPro implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session=request.getSession();
+		String M_id=(String)session.getAttribute("M_id");
 		int S_num=Integer.parseInt(request.getParameter("S_num"));
 		String D_buy=request.getParameter("D_buy");
 		
-		DealDAO dao = new DealDAO();
 		
+		DealDAO dao = new DealDAO();
+		DealDTO dto = dao.CheckDeal2(M_id, S_num);
 		ActionForward forward=null;
 
-		
+		if(dto==null) {	
 		dao.EndDeal(S_num, D_buy);
+		dao.deleteDeal(S_num);
 		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out=response.getWriter(); //프린트라이트에 쓰기 권한을 받아서 리스폰스 어쩌구에 담음
@@ -31,7 +36,18 @@ public class EndDealPro implements Action {
 		out.println("history.back()");
 		out.println("</script>");
 		out.close();	
-
+		}else{		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out=response.getWriter(); //프린트라이트에 쓰기 권한을 받아서 리스폰스 어쩌구에 담음
+		out.println("<script type='text/javascript'>");
+		out.println("alert('이미 거래가 완료된 상품입니다.')");
+		out.println("history.back()");
+		out.println("</script>");
+		out.close();	
+		forward=null;
+	 }
+			
+			
 		return forward;
 	}
 
